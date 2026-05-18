1bf98b0b115ddddabd9ef304b561e2c842b10d37
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2026-05-19 01:07:56 +0600

    Update dashboard.php

--- travel-guide/view/dashboard.php
+++ travel-guide/view/dashboard.php
@@ -1,12 +1,37 @@
-<?php session_start(); ?>
+<?php
+session_start();
 
-<link rel="stylesheet" href="../assets/style.css">
+if(!isset($_SESSION['id'])){
+    header("location: login.php");
+    exit();
+}
+
+if($_SESSION['role'] != 'scout'){
+    header("location: login.php");
+    exit();
+}
+
+if($_SESSION['is_verified'] != 1){
+    header("location: login.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <link rel="stylesheet" href="../assets/style.css">
+
+</head>
+<body>
 
 <div class="container">
 
 <h1>Scout Dashboard</h1>  
 
-
+<hr> <br><br>
 <div id="flbox">
 <div id="box">
     <a href="create.php">Create Request</a>
@@ -19,3 +44,9 @@
 </div>
 
 </div>
+<div class="btn">
+<a href="login.php"  >Logout </a>
+</div>
+</body>
+</html>
+
