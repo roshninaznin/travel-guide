48c4cdff67f8bea9a079a810dba8f5d6dc9ffae1
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2026-05-19 01:10:18 +0600

    Update my_requests.php

--- travel-guide/view/my_requests.php
+++ travel-guide/view/my_requests.php
@@ -4,9 +4,6 @@ require_once('../model/PostModel.php');
 
 $result = getMyPosts($_SESSION['id']);
 
-<?php
-session_start();
-
 if(!isset($_SESSION['id'])){
     header("location: login.php");
     exit();
